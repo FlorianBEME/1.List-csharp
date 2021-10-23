cf5e676d97ffe437e3180dbb64cb2019c8d064a6
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-10-23 16:24:18 +0200

    Ajoutez des fichiers projet.

---
+++ 1.List csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.List_csharp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> charList = new List<string>();
+
+            charList.Add("a");
+            charList.Add("z");
+            charList.Add("r");
+
+            charList.RemoveAt(0);
+            charList.Remove("r");
+            charList[0] = "e";
+        }
+    }
+}
